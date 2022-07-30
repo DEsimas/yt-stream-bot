e54656a38d275875b5e5f6a661af3db4c94f4b02
Serafim Sukharev <simasuh@gmail.com>
2022-07-30 17:29:11 +0300

    write Bot.ts

---
+++ src/Bot.ts
@@ -0,0 +1,38 @@
+import { Client, Message } from "discord.js";
+
+export interface BotOptions {
+    token: string;
+    prefix?: string;
+};
+
+export class Bot {
+    private readonly token: string;
+    private readonly prefix: string;
+    private readonly client: Client;
+
+    constructor(options: BotOptions) {
+        this.token = options.token;
+        this.prefix = options.prefix || "!";
+
+        this.client = new Client();
+
+        this.setHandlers();
+    }
+
+    public login(): void {
+        this.client.login(this.token);
+    }
+
+    private async setHandlers(): Promise<void> {
+        this.client.on("ready", () => this.readyHandler());
+        this.client.on("message", (message) => this.messageHandler(message));
+    }
+
+    private async readyHandler(): Promise<void> {
+        console.log("uwu");
+    }
+
+    private async messageHandler(message: Message): Promise<void> {
+        console.log("owo");
+    }
+};
