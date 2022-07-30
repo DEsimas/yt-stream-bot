83a76cb44cc3a28844e6ed384be9c78551e29079
Serafim Sukharev <simasuh@gmail.com>
2022-07-30 17:33:21 +0300

    bot launch in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import { config } from "dotenv";
+import { Bot } from "./Bot";
+
+config();
+
+if (!process.env.TOKEN) throw new Error("TOKEN is not provided");
+
+const bot = new Bot({
+	token: process.env.TOKEN,
+	prefix: process.env.PREFIX
+});
+
+bot.login();
