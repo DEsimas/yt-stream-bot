b359cf4f35b79870436023ab49ce10c395c003ef
Serafim Sukharev <simasuh@gmail.com>
2022-07-31 16:09:02 +0300

    fullscreen

--- src/Browser.ts
+++ src/Browser.ts
@@ -66,6 +66,10 @@ export class Browser {
 		await this.page?.keyboard.press(">");
 	}
 
+	public async fullscreen(): Promise<void> {
+		await this.page?.keyboard.press("F");
+	}
+
 	private validateURL(url: string): boolean {
 		return url.slice(0, 17) == "https://youtu.be/" ||
 			url.slice(0, 23) == "http://www.youtube.com/" ||
