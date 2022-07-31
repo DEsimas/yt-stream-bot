5660f3e02cdff19db4572736f9521861548d1631
Serafim Sukharev <simasuh@gmail.com>
2022-07-31 13:49:33 +0300

    add viewport option to Browser class

--- src/Browser.ts
+++ src/Browser.ts
@@ -1,11 +1,20 @@
 import * as Puppeteer from "puppeteer";
 
+export interface BrowserOptions {
+	viewport?: Puppeteer.Viewport;
+}
+
 export class Browser {
+	private readonly viewport: Puppeteer.Viewport;
+
 	private browser?: Puppeteer.Browser;
 	private page?: Puppeteer.Page;
 
-	constructor() {
-
+	constructor(options?: BrowserOptions) {
+		this.viewport = options?.viewport || {
+			width: 1680,
+			height: 1120
+		};
 	}
 
 	public async launch(): Promise<void> {
@@ -17,13 +26,13 @@ export class Browser {
 		if (!this.browser) throw new Error("Browser not launched");
 		this.page = await this.browser.newPage();
 		this.page.setDefaultNavigationTimeout(0);
-		this.page.setViewport({ width: 1800, height: 1010 });
+		this.page.setViewport(this.viewport);
 	}
 
 	public async openVideo(url: string): Promise<void> {
 		if (!this.page) throw new Error("Page not opened");
 		if (url.slice(0, 17) != "https://youtu.be/" || url.indexOf("?") != -1) throw new Error("Invalid video url");
 		await this.page?.goto(url);
-		setTimeout( async () => await this.page?.keyboard.press("F"), 2000);
+		setTimeout(async () => await this.page?.keyboard.press("F"), 2000);
 	}
-}
+};
