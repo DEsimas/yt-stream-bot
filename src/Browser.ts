cbdd6a8eebdaa9f642c0cd4708d81fe5bf15c255
Serafim Sukharev <simasuh@gmail.com>
2022-07-31 16:08:32 +0300

    fast

--- src/Browser.ts
+++ src/Browser.ts
@@ -62,6 +62,10 @@ export class Browser {
 		await this.page?.keyboard.press("<");
 	}
 
+	public async fast(): Promise<void> {
+		await this.page?.keyboard.press(">");
+	}
+
 	private validateURL(url: string): boolean {
 		return url.slice(0, 17) == "https://youtu.be/" ||
 			url.slice(0, 23) == "http://www.youtube.com/" ||
